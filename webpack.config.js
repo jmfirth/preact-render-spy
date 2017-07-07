c98e7c7bf2b123e04ee37c5c91202e786a2caef0
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-07-07 11:09:01 -0400

    Stop using babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = ({
       // We only want to parse our test files for JSX with babel, we want everything else to work in native
       // node!
       {
-        test: /\.test\.js$/,
+        test: /\.js$/,
         exclude: [join(__dirname, 'node_modules')],
         loader: 'babel-loader',
         options: {
-          presets: ['preact', 'env'],
+          presets: ['preact'],
         },
       },
     ],
